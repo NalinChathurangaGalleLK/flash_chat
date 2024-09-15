888ca9c572112510636362d1f43de6550ac442be
NalinChathurangaGalleLK <nalinchathurangallelk@gmail.com>
2024-09-15 10:29:07 +0530

    first commit

---
+++ lib/screens/welcome_screen.dart
@@ -0,0 +1,111 @@
+
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import '../components/rounded_button.dart';
+
+class WelcomeScreen extends StatefulWidget {
+  const WelcomeScreen({super.key});
+
+  static const String id = 'welcome_screen';
+
+  @override
+  _WelcomeScreenState createState() => _WelcomeScreenState();
+}
+
+class _WelcomeScreenState extends State<WelcomeScreen>
+    with SingleTickerProviderStateMixin {
+  AnimationController? controller;
+  Animation? animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    animation = CurvedAnimation(parent: controller!, curve: Curves.decelerate);
+
+    controller!.forward();
+
+    controller!.addListener(() {
+      setState(() {
+        animation!.value;
+      });
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    controller!.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Hero(
+                  tag: 'logo',
+                  child: SizedBox(
+                    height: animation!.value * 100,
+                    child: Image.asset('images/logo.png'),
+                  ),
+                ),
+                DefaultTextStyle(
+                  style: const TextStyle(
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.w900,
+                    color: Colors.black45,
+                  ),
+                  child: AnimatedTextKit(
+                     repeatForever: true,
+                    isRepeatingAnimation: true,
+                    
+                    animatedTexts: [
+                      
+                      ScaleAnimatedText(
+                        
+                        'Flash Chat',
+                        
+                      )
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 48.0,
+            ),
+            RoundedButton(
+              buttonColor: Colors.lightBlueAccent,
+              buttonTitle: 'Log In',
+              functionOnpressed: () {
+                Navigator.pushNamed(context, LoginScreen.id);
+              },
+            ),
+            RoundedButton(
+              buttonColor: Colors.blueAccent,
+              buttonTitle: 'Register',
+              functionOnpressed: () {
+                Navigator.pushNamed(context, RegistrationScreen.id);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
